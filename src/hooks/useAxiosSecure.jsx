695358349a7abacc36fda2c21264459f276f3673
MRiDuL-ICE <abdulwahab22400@gmail.com>
2024-12-18 10:59:23 +0600

    configure axios interceptor

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,4 +1,7 @@
 import axios from "axios";
+import { useEffect } from "react";
+import Swal from "sweetalert2";
+import useAuth from "./useAuth";
 
 const axiosInstance = axios.create({
   baseURL: "http://localhost:3000",
@@ -6,6 +9,34 @@ const axiosInstance = axios.create({
 });
 
 const useAxiosSecure = () => {
+  const { userLogout } = useAuth();
+  useEffect(() => {
+    axiosInstance.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (error) => {
+        console.log("error caught in interceptor", error);
+
+        if (error.status === 401 || error.status === 403) {
+          userLogout()
+            .then((res) => {
+              Swal.fire({
+                title: "Unauthorized access!",
+                text: "You have been logged out!",
+                icon: "error",
+              });
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        }
+
+        return Promise.reject(error);
+      }
+    );
+  }, []);
+
   return axiosInstance;
 };
 
